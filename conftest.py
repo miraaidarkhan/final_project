a2cb23a5a7cddb9a089606a4451cb111330fec19
Meruyert <mika10kzm@gmail.com>
2023-06-15 16:52:06 +0600

    исправлены файлы

--- conftest.py
+++ conftest.py
@@ -8,17 +8,9 @@ def pytest_addoption(parser):
                      help="language")
 
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
